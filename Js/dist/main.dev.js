cba9ba9a9912883f6a043d345ad36e54b61db36b
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-04-05 12:39:55 +0100

    deploys

--- Js/dist/main.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').then(function (reg) {
-    return console.log('Service Worker Initialised', reg);
-  })["catch"](function (err) {
-    return console.log(err);
-  });
-}
