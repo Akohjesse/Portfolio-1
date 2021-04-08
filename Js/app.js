7a599d3652e038802eb005e9a06239c11a248d7a
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-04-08 23:52:55 +0100

    Deploys

--- Js/app.js
+++ Js/app.js
@@ -1,23 +1,8 @@
 this.addEventListener("load", preloader);
 function preloader() {
-	var loader = document.querySelector(".loader");
-	setTimeout(5000, del());
-
-	function del() {
-		loader.classList.add("animate__animated", "animate__slideOutLeft", "animate__delay-1s");
-	}
-	var loader2 = document.querySelector(".loader2");
-	setTimeout(5000, del2());
-
-	function del2() {
-		loader2.classList.add("animate__animated", "animate__slideOutUp", "animate__delay-4s");
-	}
-	var loader3 = document.querySelector(".loader3");
-	setTimeout(5000, del3());
-
-	function del3() {
-		loader3.classList.add("animate__animated", "animate__slideOutRight", "animate__delay-3s");
-	}
+	setTimeout(()=>{document.querySelector('.loader').classList.add('animate__fadeOut')}, 2000)
+		
+	
 	setLS();
 	
 }
